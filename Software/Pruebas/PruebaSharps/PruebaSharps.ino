f53a5b7ccbeb14b903c31893c751a405d0a539ac
LeandroBenitez12 <LeandroBenitez12@users.noreply.github.com>
2023-10-04 01:17:59 -0300

    pruebo sharps!

--- Software/Pruebas/PruebaSharps/PruebaSharps.ino
+++ Software/Pruebas/PruebaSharps/PruebaSharps.ino
@@ -2,36 +2,57 @@
 #include <DistanceSensors.h>
 #include "BluetoothSerial.h"
 
-#define PIN_SENSOR_DERECHO 25
-#define PIN_SENSOR_IZQUIERDO 35
-float distancia_derecha;
-float distancia_izquierda;
+
+#define PIN_SHARP_LEFT 25
+#define PIN_SHARP_CENTER_LEFT 33
+#define PIN_SHARP_CENTER_RIGHT 32
+#define PIN_SHARP_RIGHT 35
+int distSharpCenterLeft = 0;
+int distSharpCenterRight = 0;
+int distSharpLeft = 0;
+int distSharpRight = 0;
 
 unsigned long currentTime = 0;
-#define TICK_DEBUG 1000
+#define TICK_DEBUG 700
 
 #if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
 #error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
 #endif
 
 BluetoothSerial SerialBT;
-Isensor *sharp_derecha = new Sharp_GP2Y0A02(PIN_SENSOR_DERECHO);
-Isensor *sharp_izquierda = new Sharp_GP2Y0A02(PIN_SENSOR_IZQUIERDO);
+
+Isensor *sharp_left = new Sharp_GP2Y0A02(PIN_SHARP_LEFT);
+Isensor *sharp_center_left = new Sharp_GP2Y0A02(PIN_SHARP_CENTER_LEFT);
+Isensor *sharp_center_right = new Sharp_GP2Y0A02(PIN_SHARP_CENTER_RIGHT);
+Isensor *sharp_right = new Sharp_GP2Y0A02(PIN_SHARP_RIGHT);
+
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
   SerialBT.begin("Sami");
+}
+
+void printRead(){
+  if (millis() > currentTime + TICK_DEBUG)
+  {
+    SerialBT.print("Left Distance: ");
+    SerialBT.println(distance_left);
+    SerialBT.print("Left Center Distance: ");
+    SerialBT.println(distance_center_left);
+    SerialBT.print("right Distance: ");
+    SerialBT.println(distance_right);
+    SerialBT.print("right Center Distance: ");
+    SerialBT.println(distance_center_right);
+  }
 }
 
 void loop() {
   
+  
+  distance_left = sharp_left->SensorRead();
+  distance_center_left = sharp_center_left->SensorRead();
+  distance_center_right = sharp_center_right->SensorRead(); 
+  distance_right  = sharp_right->SensorRead();
   distancia_derecha = sharp_derecha->SensorRead();
-  distancia_izquierda = sharp_izquierda->SensorRead();
-
-  if (millis() > currentTime + TICK_DEBUG)
-  {
-    SerialBT.print("Distancia derecha: ");
-    SerialBT.println(distancia_derecha);
-    SerialBT.print("Distancia Izquierda: ");
-    SerialBT.println(distancia_izquierda);
-  }
+ 
+  printRead();
 }
