116e35775d72c15cbdf74606d8d62d90e9fc65f6
LeandroBenitez12 <LeandroBenitez12@users.noreply.github.com>
2023-10-04 11:56:42 -0300

    Subo mi codigo

--- Software/Pruebas/PruebaMotores/PruebaMotores.ino
+++ Software/Pruebas/PruebaMotores/PruebaMotores.ino
@@ -1,3 +1,5 @@
+//librerias
+#include "BluetoothSerial.h"
 #include <EngineController.h>
 
 #define PIN_RIGHT_ENGINE_IN1 21
@@ -10,24 +12,36 @@
 #define PWM_CHANNEL_LEFT_IN2 4
 int speed = 200;
 
+//configuramos el Serial Bluetooth
+#if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
+#error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
+#endif
+BluetoothSerial SerialBT;
+
 IEngine *rightEngine = new Driver_DRV8825(PIN_RIGHT_ENGINE_IN1, PIN_RIGHT_ENGINE_IN2, PWM_CHANNEL_RIGHT_IN1, PWM_CHANNEL_RIGHT_IN2);
 IEngine *leftEngine = new Driver_DRV8825(PIN_LEFT_ENGINE_IN1, PIN_LEFT_ENGINE_IN2, PWM_CHANNEL_LEFT_IN1, PWM_CHANNEL_LEFT_IN2);
 EngineController *Ryo = new EngineController(rightEngine, leftEngine);
 void setup()
 {
+    SerialBT.begin("Test Motores");
 }
 
 void loop()
 {
     robot->Forward(speed);
+    SerialBT.println("Forward ")
     delay(3000);
     robot->Backward(speed);
+    SerialBT.println("Backward ")
     delay(3000);
     robot->Left(speed);
+    SerialBT.println("Left ")
     delay(3000);
     robot->Right(speed);
+    SerialBT.println("Right ")
     delay(3000);
     robot->Stop();
+    SerialBT.println("Stop ")
     delay(3000);
   
 }
