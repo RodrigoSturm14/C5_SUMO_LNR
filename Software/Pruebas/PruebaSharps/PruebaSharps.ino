45186c070ecde3ccb5ec9d14e392ee650b19de49
LeandroBenitez12 <LeandroBenitez12@users.noreply.github.com>
2023-10-04 02:03:46 -0300

    lectura joya la verdad

--- Software/Pruebas/PruebaSharps/PruebaSharps.ino
+++ Software/Pruebas/PruebaSharps/PruebaSharps.ino
@@ -2,18 +2,17 @@
 #include <DistanceSensors.h>
 #include "BluetoothSerial.h"
 
-
 #define PIN_SHARP_LEFT 25
 #define PIN_SHARP_CENTER_LEFT 33
 #define PIN_SHARP_CENTER_RIGHT 32
 #define PIN_SHARP_RIGHT 35
-int distSharpCenterLeft = 0;
-int distSharpCenterRight = 0;
-int distSharpLeft = 0;
-int distSharpRight = 0;
+int distance_left = 0;
+int distance_center_left = 0;
+int distance_center_right = 0;
+int distance_right = 0;
 
 unsigned long currentTime = 0;
-#define TICK_DEBUG 700
+#define TICK_DEBUG 2000
 
 #if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
 #error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
@@ -28,7 +27,7 @@ Isensor *sharp_right = new Sharp_GP2Y0A02(PIN_SHARP_RIGHT);
 
 void setup() {
   Serial.begin(115200);
-  SerialBT.begin("Sami");
+  SerialBT.begin("Aldosivi");
 }
 
 void printRead(){
@@ -42,6 +41,7 @@ void printRead(){
     SerialBT.println(distance_right);
     SerialBT.print("right Center Distance: ");
     SerialBT.println(distance_center_right);
+    SerialBT.println("-----------------------");
   }
 }
 
@@ -52,7 +52,6 @@ void loop() {
   distance_center_left = sharp_center_left->SensorRead();
   distance_center_right = sharp_center_right->SensorRead(); 
   distance_right  = sharp_right->SensorRead();
-  distancia_derecha = sharp_derecha->SensorRead();
  
   printRead();
 }
